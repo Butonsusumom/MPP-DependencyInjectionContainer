fc915b48ad78a8f21d59f46b2b253e1690ebb19a
Ksenia Tsubulko <tsubulko.ksenia@gmail.com>
2020-11-17 09:17:21 +0300

    Main Class

--- DependencyInjectionContainer/DependenciesConfiguration.cs
+++ DependencyInjectionContainer/DependenciesConfiguration.cs
@@ -16,64 +16,7 @@ namespace DependencyInjectionContainer
             Register(typeof(TDependency), typeof(TImplementation), isSingleton, name);
         }
 
-        public void Register(Type dependency, Type implementation, bool isSingleton = false, string name = null)
-        {
-            if (dependency.IsGenericTypeDefinition ^ implementation.IsGenericTypeDefinition)
-            {
-                throw new ArgumentException("Open generics register should be with both open generic types");
-            }
 
-            if (dependency.IsGenericTypeDefinition)
-            {
-                if (isSingleton)
-                {
-                    throw new ArgumentException("Open generic cannot be singleton");
-                }
-
-                if (!dependency.IsAssignableFromAsOpenGeneric(implementation))
-                {
-                    throw new ArgumentException("Dependency is not assignable from implementation");
-                }
-            }
-            else
-            {
-                if (!dependency.IsClass && !dependency.IsAbstract && !dependency.IsInterface 
-                    || (!implementation.IsClass || implementation.IsAbstract))
-                {
-                    throw new ArgumentException("Wrong types");
-                }
-
-                if (!dependency.IsAssignableFrom(implementation))
-                {
-                    throw new ArgumentException("Dependency is not assignable from implementation");
-                }
-            }
-
-            ImplementationContainer container = new ImplementationContainer(implementation, isSingleton, name);
-            if (dependency.IsGenericType)
-            {
-                dependency = dependency.GetGenericTypeDefinition();
-            }
-
-            List<ImplementationContainer> dependencyImplementations;
-            lock (implementations)
-            {
-                if (!implementations.TryGetValue(dependency, out dependencyImplementations))
-                {
-                    dependencyImplementations = new List<ImplementationContainer>();
-                    implementations[dependency] = dependencyImplementations;
-                }
-            }
-
-            lock (dependencyImplementations)
-            {
-                if (name != null)
-                {
-                    dependencyImplementations.RemoveAll((existingContainer) => existingContainer.Name == name);
-                }
-                dependencyImplementations.Add(container);
-            }
-        }
 
         public IEnumerable<ImplementationContainer> GetImplementations(Type type)
         {
